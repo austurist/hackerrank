0400213e1ea51ec1f6abd1893150385e5abca55e
Austurist <austurist@gmail.com>
2022-03-08 20:46:57 +0100

    inversion count - good but slow

--- sorting/src/main/java/inversions/Solution.java
+++ sorting/src/main/java/inversions/Solution.java
@@ -22,8 +22,16 @@ class Result {
      */
 
     public static long countInversions(List<Integer> arr) {
-        // Write your code here
-        return 0L;
+        long inversions = 0;
+
+        for (int i = 0; i < arr.size()-1; ++i) {
+            for (int j = i+1; j < arr.size(); ++j) {
+                if (arr.get(i) > arr.get(j))
+                    inversions++;
+            }
+        }
+
+        return inversions;
     }
 
 }
