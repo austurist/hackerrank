840a9a91f1e8a934804aca8ab9a8c9d5b70d5ff4
Austurist <austurist@gmail.com>
2022-07-03 16:52:53 +0200

    string manipulation, alternating

---
+++ stringmanip/src/main/java/alternating/Solution.java
@@ -0,0 +1,58 @@
+package alternating;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    public static int alternatingCharacters(String s) {
+        int todelete = 0;
+        var chars = s.toCharArray();
+        char last = chars[0];
+        for (int i = 1; i < chars.length; ++i) {
+            char current = chars[i];
+            if (current == last) {
+                // not alternating, have to delete
+                todelete++;
+            }
+
+            last = current;
+        }
+        return todelete;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, q).forEach(qItr -> {
+            try {
+                String s = bufferedReader.readLine();
+
+                int result = Result.alternatingCharacters(s);
+
+                bufferedWriter.write(String.valueOf(result));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
